94ffe6298c4abadd41c634e2f9a1e8e1549fbcc9
callen <callen@git.aarons.com>
2018-07-14 21:50:19 -0400

    add array to book list

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -13,6 +13,11 @@ export default {
   data() {
     return {
       title: 'All Books',
+      books: [
+    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+    {title: 'American Gods', author: 'Neil Gaiman'},
+    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+]
     };
   },
 };
